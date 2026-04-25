f7fa6dc0c0a8712518b42e54eff44b589b8e1bd9
Radiantwaves <144567322+Radiantwaves@users.noreply.github.com>
2026-04-25 21:16:22 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,35 +1,24 @@
-async function loadNews() {
-  const res = await fetch("news.json");
-  const data = await res.json();
+function loadNews() {
 
-  // HERO
-  document.getElementById("hero-title").textContent = data.main.title;
+  const data = JSON.parse(localStorage.getItem("news"));
 
-  // BREAKING
+  if (!data) return;
+
+  document.getElementById("hero-title").textContent = data.main.title || "";
+
   document.getElementById("breaking-text").textContent = data.breaking;
 
-  // MAIN ARTICLE
   document.getElementById("main-news").innerHTML = `
     <h2>${data.main.title}</h2>
     <img src="${data.main.image}">
     <p>${data.main.description}</p>
   `;
 
-  // SIDEBAR
   const sidebar = document.getElementById("sidebar-news");
   sidebar.innerHTML = "";
 
   data.topStories.forEach(story => {
-    sidebar.innerHTML += `<article><h4>${story.title}</h4></article>`;
-  });
-
-  // VIDEO
-  const videoSection = document.getElementById("video-section");
-  data.videos.forEach(video => {
-    videoSection.innerHTML += `
-      <h3>${video.title}</h3>
-      <iframe src="${video.url}" frameborder="0" allowfullscreen></iframe>
-    `;
+    sidebar.innerHTML += <article><h4>${story.title}</h4></article>;
   });
 }
 

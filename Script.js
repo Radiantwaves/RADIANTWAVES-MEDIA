92d8c72cf0a050b5349847135759a0d5d57593d5
Radiantwaves <144567322+Radiantwaves@users.noreply.github.com>
2026-05-09 17:55:56 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,39 +3,57 @@ fetch("news.json")
 .then(data => {
 
   // BREAKING NEWS
-  document.getElementById("breaking-ticker").innerHTML =
-    data.breaking.join(" 🔴 ");
+  document.getElementById("breaking-ticker")
+  .innerHTML = data.breaking.join(" 🔴 ");
 
   // HERO
-  document.getElementById("hero-title").innerText =
-    data.articles[0].title;
 
-  document.getElementById("hero-desc").innerText =
-    data.articles[0].description;
+  document.getElementById("hero-title")
+  .innerText = data.articles[0].title;
+
+  document.getElementById("hero-desc")
+  .innerText = data.articles[0].description;
 
   // FEATURED
-  const featured = document.getElementById("featured-news");
+
+  const featured =
+  document.getElementById("featured-news");
 
   featured.innerHTML = `
+
     <div class="featured-card">
+
       <img src="${data.articles[0].image}">
+
       <div>
+
         <h2>${data.articles[0].title}</h2>
 
         <p>${data.articles[0].content}</p>
+
       </div>
+
     </div>
+
   `;
 
-  // CATEGORY FUNCTION
+  // RENDER FUNCTION
+
   function render(category, id){
 
-    const container = document.getElementById(id);
+    const container =
+    document.getElementById(id);
 
     const filtered =
-      data.articles.filter(
-        a => a.category.toLowerCase() === category
-      );
+    data.articles.filter(
+      a => a.category.toLowerCase() === category
+    );
+
+    let videos = [
+      "assets/video1.mp4",
+      "assets/video2.mp4",
+      "assets/video3.mp4"
+    ];
 
     filtered.forEach((news, index) => {
 
@@ -51,7 +69,9 @@ fetch("news.json")
 
             <p>${news.description}</p>
 
-            <p>${news.content.substring(0,200)}...</p>
+            <p>
+              ${news.content.substring(0,200)}...
+            </p>
 
           </div>
 
@@ -59,15 +79,23 @@ fetch("news.json")
 
       `;
 
-      // INSERT VIDEO AFTER EVERY 3 NEWS
+      // VIDEO AFTER EVERY 3 NEWS
+
       if((index + 1) % 3 === 0){
+
+        let videoIndex =
+        Math.floor(index / 3) % videos.length;
 
         container.innerHTML += `
 
           <div class="video-card">
 
-            <video controls autoplay muted loop>
-              <source src="assets/video2.mp4" type="video/mp4">
+            <video autoplay muted loop controls>
+
+              <source
+              src="${videos[videoIndex]}"
+              type="video/mp4">
+
             </video>
 
           </div>
@@ -76,6 +104,7 @@ fetch("news.json")
       }
 
     });
+
   }
 
   render("world", "world-news");
